aad0df294690341e952974008fc9cd36934ed5b1
JinsYin <jinsyin@163.com>
2024-05-07 16:44:34 +0800

    :sparkles: Support the knife4j documentation and the h2 datasource

--- src/main/java/cn/guruguru/javatemplate/controller/GreetController.java
+++ src/main/java/cn/guruguru/javatemplate/controller/GreetController.java
@@ -1,10 +1,15 @@
 package cn.guruguru.javatemplate.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Greet")
 @RestController // @Controller + @ResponseBody
 @RequestMapping(path = "/greet")
 public class GreetController {
@@ -16,6 +21,10 @@ public class GreetController {
      * @return a greeting
      */
     @GetMapping("/hello")
+    @ApiOperation("Hello")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "person", value = "The person to greet")
+    })
     public static String hello(@RequestParam("person") String person) {
         return "Hello, " + person;
     }
