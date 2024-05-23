db3cf424535965b52727dbc87c6da02673dbbfdb
JinsYin <jinsyin@163.com>
2024-05-23 10:33:55 +0800

    :building_construction: Add a Bean layer

---
+++ src/main/java/cn/guruguru/template/bean/config/BeanConfig.java
@@ -0,0 +1,9 @@
+package cn.guruguru.template.bean.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "cn.guruguru.template.bean" })
+public class BeanConfig {
+}
