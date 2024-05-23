cd98b6f41dd95ef6fb3792f86a18d306b5c5259d
JinsYin <jinsyin@163.com>
2024-05-23 10:33:55 +0800

    :building_construction: Add a Bean layer

---
+++ src/main/java/cn/guruguru/template/bean/service/RedisConfig.java
@@ -0,0 +1,9 @@
+package cn.guruguru.template.bean.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class RedisConfig {
+}
